9040de5b9c0a067530563e0227654878474057bc
Pekomon <5183385+pekomon@users.noreply.github.com>
2020-03-23 11:11:35 +0200

    Set up UI navigation components

---
+++ app/src/main/java/com/example/pekomon/weatherapp/UI/settings/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.pekomon.weatherapp.UI.settings
+
+import androidx.fragment.app.Fragment
+
+// Todo: use PreferenceFragmentCompat later
+class SettingsFragment() : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/pekomon/weatherapp/UI/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pekomon.weatherapp.UI.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
